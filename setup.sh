fe2e4941b0e4e1aa71603d0ab28f9c215131ad3c
kapi00z <62167567+kapi00z@users.noreply.github.com>
2021-06-03 03:50:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,78 @@
 #!/bin/bash
 
-echo "Provide host number: "
-read host
+argarr=(2 4 6)
 
-hostnamectl set-hostname "arch${host}"
-sed -i "s/arch/arch${host}/g" /etc/hosts
+if [[ ! "${argarr[@]}" =~ "${argnum}" ]]
+then
+    echo "Incorrect arguments number!"
+    exit 0
+fi
+
+while getopts ":h:a:" opt
+do
+    case "${opt}" in
+        h)
+            opthost=${OPTARG}
+            ;;
+        a)
+            optaddr=${OPTARG}
+            ;;
+    esac
+done
+
+if [[ "${opthost}" == "" ]]
+then
+    echo "Enter hostname: "
+    read host
+else
+    host=${opthost}
+fi
+
+if [[ "${optaddr}" == "" ]]
+then
+    echo "Enter address: "
+    read addr
+else
+    addr=${optaddr}
+fi
+
+#numhost=$(echo "${host}" | sed 's/[^0-9]//g')
+
+echo "Number: ${addr}"
+echo "Name: ${host}"
+
+if [[ "$host" == "" ]]
+then
+    echo "Empty hostname entered, exiting"
+    exit 0
+elif [[ "$addr" == "" ]]
+then
+    echo "Cannot generate address, exiting"
+    exit 0
+elif [[ "$addr" -lt "31" || "$addr" -gt "255" ]]
+then
+    echo "Address not in range"
+    exit 0
+fi
+
+hostnamectl set-hostname "${host}"
+sed -i "s/arch/${host}/g" /etc/hosts
 
 if [[ ! $(sudo cat /etc/dhcpcd.conf | grep -e "static " | wc -l) -eq "0" ]]
 then
     echo "Static IP already!"
-    export addr=$(cat /etc/dhcpcd.conf | grep -e "ip_address" | sed 's/[\/]/./' | cut -d "." -f 4)
-    if [[ "$addr" == "3${host}" ]]
+    export curaddr=$(cat /etc/dhcpcd.conf | grep -e "ip_address" | sed 's/[\/]/./' | cut -d "." -f 4)
+    if [[ "$curaddr" == "${host}" ]]
     then
         echo "Static IP address correct!"
     else
-        sed -i "s/${addr}/3${host}/" /etc/dhcpcd.conf
+        sed -i "s/${curaddr}/${addr}/" /etc/dhcpcd.conf
     fi
 else
     cat << EOF >> /etc/dhcpcd.conf
 
 interface enp0s3
-static ip_address=192.168.1.3${host}/24
+static ip_address=192.168.1.${addr}/24
 static routers=192.168.1.1
 static domain_name_servers=192.168.1.1 8.8.4.4
 EOF
