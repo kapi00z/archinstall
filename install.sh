4d04ef439ba466f3956388135bfc26b928e763c3
kapi00z <62167567+kapi00z@users.noreply.github.com>
2021-06-03 05:16:34 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+autoPart() {
+    fdisk -l
+    echo -n "Choose partition: "
+    read disk
+
+    cat << EOF | fdisk $disk
+    g
+    n
+    +512M
+    n
+    t
+    1
+    1
+    w
+EOF
+
+    mkfs.vfat ${disk}1
+    mkfs.ext4 ${disk}2
+
+    mount ${disk}2 /mnt
+    mkdir /mnt/boot
+    mount ${disk}1 /mnt/boot
+}
+
+echo -n "Set your hostname: "
+read host
+
+disk='/dev/sda'
+
+timedatectl set-ntp true
+
+#echo -n "Do you want automatic(a) or manual(m) disk partitioning and formatting (default is automatic): "
+#read part
+#
+#case $part in
+#
+    #a)
+        #autoPart
+        #;;
+#        
+    #"")
+        #autoPart
+        #;;
+#        
+    #m)
+        #fdisk -l
+        #fdisk
+        #;;
+#
+    #*)
+        #echo "Incorrect input, ending script"
+        #exit 0
+        #;;
+#
+#esac
+
+autoPart
+
+pacstrap /mnt base linux linux-firmware dhcpcd grub efibootmgr
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "${host}" > /mnt/etc/hostname
+
+cat << EOF > /mnt/etc/hosts
+127.0.0.1       localhost
+::1             localhost
+127.0.1.1       ${host}.localdomain ${host}
+EOF
+
+#sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /mnt/locale.gen
+#arch-chroot /mnt locale-gen
+
+arch-chroot /mnt systemctl enable dhcpcd
+
+echo "Set your root password: "
+arch-chroot /mnt passwd
+
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+curl https://raw.githubusercontent.com/kapi00z/archinstall/master/setup.sh > /mnt/root/setup.sh
